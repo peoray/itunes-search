3639b0b8ee1f86511ebcfb74e2d8f739880a48aa
peoray <peoray@yahoo.com>
2018-07-23 01:58:52 +0100

    Add Readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+#Itunes-Search
+
+How to run the app locally:
+
+1. Run <code> npm install </code> to install all needed dependencies
+
+1. Then start the server using `node app.js`
+
+1. Navigate to your browser <code> http://localhost:3000/ </code> to view the app 
--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+// dependencies for the app 
 const express = require('express');
 const request = require('request');
-const ejs = require('ejs');
 const bodyParser = require('body-parser');
+
 const app = express();
 
+// express middlewares
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
 
+// route to render index page
 app.get('/', (req, res) => res.render('index'));
 
+// route to get the results of the search term
 app.get('/results', (req, res) => {
+  // gets search term from index page
   const searchTerm = req.query.search;
   const url = `https://itunes.apple.com/search?term=${searchTerm}&entity=album`;
-  request(url, function(error, response, body) {
+  request(url, function (error, response, body) {
     console.log('error:', error);
     const data = JSON.parse(body);
-    console.log('body:', data);
-    res.render('results', { data });
+    res.render('results', {
+      data
+    });
   });
 });
 
+// connect app to localhost port
 app.listen(3000, () => console.log('serving is listening on port 3000'));
